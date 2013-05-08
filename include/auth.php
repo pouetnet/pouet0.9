5d2bf0d7f489614a16ae3118564d7ff44cdb419a
Laurent Raufaste <analogue@glop.org>
2013-05-07 20:13:03 -0400

    Added a fake credentials file

---
+++ include/auth.php
@@ -0,0 +1,16 @@
+<?php
+// Database
+$db['host']     = 'localhost';
+$db['user']     = 'pouet';
+$db['password'] = 'pouet';
+$db['database'] = 'pouet';
+
+$sceneidUrl = "http://localhost/some/endpoint";
+$sceneidLogin = "pouet";
+$sceneidPassword = md5("pouet");
+
+define('RECAPTCHA_PUB_KEY', 'pouet');
+define('RECAPTCHA_PRIV_KEY', 'pouet');
+
+define('RECAPTCHA_MAILHIDE_PUB_KEY', 'pouet');
+define('RECAPTCHA_MAILHIDE_PRIV_KEY', 'pouet');
